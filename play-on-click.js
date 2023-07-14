4fb04f50a66554880103165043f0195bf44c5a0b
dpapanicker <70652939+dpapanicker@users.noreply.github.com>
2023-07-14 16:51:35 +0530

    Add files via upload

---
+++ play-on-click.js
@@ -0,0 +1,18 @@
+AFRAME.registerComponent("play-on-click", {
+ //Add code here
+  schema :{
+
+  },
+
+  init: function (){
+
+  },
+
+  play: function(){
+
+  },
+
+  onClick: function(){
+    
+  }
+});
